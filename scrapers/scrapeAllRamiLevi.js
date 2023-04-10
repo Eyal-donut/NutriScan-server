656c5c01bad271bae4dd006fe5cc0481ada35212
Eyal-donut <LilaEyal@gmail.com>
2023-04-10 13:56:10 +0300

    1

--- scrapers/scrapeAllRamiLevi.js
+++ scrapers/scrapeAllRamiLevi.js
@@ -23,7 +23,7 @@ const performScraping = async () => {
   while (true) {
     try {
       await page.goto(
-        `https://www.rami-levy.co.il/he/online/market/%D7%9E%D7%A9%D7%A7%D7%90%D7%95%D7%AA`
+        `https://www.rami-levy.co.il/he/online/market/%D7%97%D7%98%D7%99%D7%A4%D7%99%D7%9D-%D7%95%D7%9E%D7%AA%D7%95%D7%A7%D7%99%D7%9D`
       );
       // await page.waitForSelector(selectors.categoryHeaderSelector);
       await page.waitForSelector(selectors.imageSelector);
