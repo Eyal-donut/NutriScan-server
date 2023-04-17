7112d4e3f59e553ccb18fa3adb6475837984cdb2
Eyal-donut <LilaEyal@gmail.com>
2023-04-17 16:56:11 +0300

    /

--- controllers/localProductsController.js
+++ controllers/localProductsController.js
@@ -1,6 +1,6 @@
 import fs from "fs";
 import { translateApi } from "../utils/translateApi.js";
-import {translateNutValues} from "../utils/translateNutValues.js"
+import { translateNutValues } from "../utils/translateNutValues.js";
 
 const createProductLocally = (prod) => {
   const products = loadProducts();
@@ -28,9 +28,30 @@ const saveProducts = (products) => {
   fs.writeFileSync("products.json", dataJSON);
 };
 
-const translateAndCreateJson = async (productJson) => {
+
+
+const test = {
+  "category": "גבינת שמנת,מותכת",
+  "imageURL": "https://www.rami-levy.co.il/_ipx/w_245,f_webp/https://img.rami-levy.co.il/product/7290000045580/small.jpg",
+  "ingredients": "גבינות חצי קשות (55%) (חלב), מים, חמאה, רכיבי חלב, מלחים מתחלבים (E-450, E-452), מלח, חומצת מאכל (E-330), צבע מאכל: אנאטו (ממקור צמחי), חומר משמר (E-202).",
+  "barcode": 7290000045580,
+  "name": "גבינה מותכת משולשים",
+  "company": "נעמה",
+  "nutritionalValues": "297\nקלוריות\nאנרגיה\n25\nגרם\nשומנים\n15\nגרם\nחומצות שומן רוויות\n0.9\nגרם\nחומצות שומן טראנס\n75\nמג\nכולסטרול\n1250\nמג\nנתרן\n2\nגרם\nסך הפחמימות\n16\nגרם\nחלבונים\n540\nמג\nסידן"
+}
+
+
+const translateAndCreateJson = async (product) => {
   try {
-    let { category, name, company, ingredients, nutritionalValues } = JSON.parse(product);
+
+    let { category, name, company, ingredients, nutritionalValues } = product
+      
+    let editedProduct = {};
+
+    if (nutritionalValues) {
+      editedProduct.nutritionalValues = translateNutValues(nutritionalValues);
+    }
+
     if (!category) {
       category = "Missing information";
     }
@@ -49,11 +70,13 @@ const translateAndCreateJson = async (productJson) => {
       company,
       ingredients
     );
-    if (nutritionalValues){
 
-    }
-
-  } catch (error) {}
+    editedProduct = { ...editedProduct, ...translateInfo };
+    console.log(editedProduct)
+  } catch (error) {
+    console.error(error)
+  }
 };
+
 
 export { createProductLocally };
