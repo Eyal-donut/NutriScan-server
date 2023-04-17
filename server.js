c673f6b4866a9f072cdcbfd676388bb0b4b334e3
Eyal-donut <LilaEyal@gmail.com>
2023-04-17 17:47:44 +0300

    newDB

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.use("/api/v1/auth", authRouter);
 
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 
 let server;
 connectDB().then(() => {
